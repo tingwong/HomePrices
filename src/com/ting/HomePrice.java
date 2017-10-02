b6e5f50119ca937e26ffbd4e9e6045bdbadcc022
Ting Wong <ting.wong@nordstrom.com>
2017-10-01 20:53:30 -0700

    Ayyy! Streamed it (with help).

--- src/com/ting/HomePrice.java
+++ src/com/ting/HomePrice.java
@@ -2,8 +2,6 @@ package com.ting;
 
 import java.util.*;
 
-import static java.lang.Math.*;
-
 //  HOMEWORK:   A city object contains an avg home price and longitude/latitude coordinates of the city.
 //              Given a list of people with latitude and longitude of their homes, determine the avg home
 //              price of the people in the list by finding their nearest city
@@ -13,6 +11,49 @@ public class HomePrice {
     // TW:  First time using HashSet -- is it a good choice for a data structure?
     //      My rationale is to keep the cities unique
     private HashSet<City> cityList = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        HomePrice homePrice = new HomePrice();
+
+        // Making list of people
+        List<Person> friendsInFarawayLands = new ArrayList<>();
+
+        LatLong latLong1 = new LatLong(57.077455, -2.775336);
+        LatLong latLong2 = new LatLong(46.862496, 103.846656);
+        LatLong latLong3 = new LatLong(31.539538, -110.756196);
+
+        Person harryPotter = new Person("Harry", latLong1);
+
+        Person tuvanThroatSinger = new Person("Mr. Vibrato", latLong2);
+        Person doraTheExplorer = new Person("Dora", latLong3);
+
+        friendsInFarawayLands.add(harryPotter);
+        friendsInFarawayLands.add(tuvanThroatSinger);
+        friendsInFarawayLands.add(doraTheExplorer);
+
+        // Making list of cities
+
+        LatLong torontoLatLong = new LatLong(43.653226, -79.383184);
+        LatLong seattleLatLong = new LatLong(47.606209, -122.332071);
+        LatLong kashmirLatLong = new LatLong(33.778175, 76.576171);
+
+        homePrice.addCity("Toronto", 1000000, torontoLatLong);
+        homePrice.addCity("Seattle", 700000, seattleLatLong);
+        homePrice.addCity("Kashmir", 130000, kashmirLatLong);
+
+        LatLong city1 = new LatLong(57, -2);
+        LatLong city2 = new LatLong(46, 103);
+        LatLong city3 = new LatLong(31, -110);
+
+        homePrice.findNearestCity(city1);
+        homePrice.findNearestCity(city2);
+        homePrice.findNearestCity(city3);
+
+        double avgPrice = homePrice.calculateAvgHomePrice(friendsInFarawayLands);
+        System.out.println(avgPrice);
+
+    }
 
     // TW:  In this instance, I'd actually like to have addCity be static, but then it can't keep
     //      track of what's currently in the list. But then, I have to instantiate HomePrice each
@@ -31,26 +72,33 @@ public class HomePrice {
 
         HashMap<City, Double> distanceToCity = new HashMap<>();
 
-        // Iterate through all the cities, calculating the distance between home and city
+        // Iterate through all the cities, calculating the distance between home and city and add to HashMap
         for (City currentCity : cityList) {
 
             double distanceBetweenCity = currentCity.getLatLong().calculateDistanceTo(latLong);
-
             distanceToCity.put(currentCity, distanceBetweenCity);
+
         }
 
-        // Set the initial smallest distance and nearest city with the first city's values
-        double smallestDistance = Double.MAX_VALUE;
-        City nearestCity = null;
+//        // Set the initial smallest distance and nearest city with the first city's values
+//        double smallestDistance = Double.MAX_VALUE;
+//        City nearestCity = null;
+//
+//        for (Map.Entry<City, Double> currentEntry : distanceToCity.entrySet()) {
+//
+//            if ((double) currentEntry.getValue() < smallestDistance) {
+//
+//                smallestDistance = (double) currentEntry.getValue();
+//                nearestCity = (City) currentEntry.getKey();
+//            }
+//        }
 
-        for (Map.Entry<City, Double> currentEntry : distanceToCity.entrySet()) {
 
-            if ((double) currentEntry.getValue() < smallestDistance) {
-
-                smallestDistance = (double) currentEntry.getValue();
-                nearestCity = (City) currentEntry.getKey();
-            }
-        }
+        // Try it with streams
+        City nearestCity =  distanceToCity.entrySet().stream()
+                .min((a, b) -> a.getValue().compareTo(b.getValue()))
+                .get()
+                .getKey();
 
         return nearestCity;
 
@@ -58,69 +106,20 @@ public class HomePrice {
 
     public double calculateAvgHomePrice(List<Person> people) {
 
-        List<Double> cityPrices = new ArrayList<>();
 
-        // Iterate through list of people, find nearest city and add that city's average home price
-        // to an ArrayList
+        double totalHomePrice = 0;
+
+        // Iterate through list of people, get sum total of all average home prices
+
         for (int i = 0; i < people.size(); i++) {
 
             double cityAvgHomePrice = findNearestCity(people.get(i).getLatlong()).getAvgHomePrice();
-            cityPrices.add(cityAvgHomePrice);
+            totalHomePrice += cityAvgHomePrice;
         }
 
-        double sumTotalHomePrice = 0;
-
-        // Calculate average home price
-        for (int i =0; i < cityPrices.size(); i++) {
-            sumTotalHomePrice += cityPrices.get(i);
-        }
-
-        double avgHomePrice = sumTotalHomePrice / cityPrices.size();
+        double avgHomePrice = totalHomePrice / people.size();
 
         return avgHomePrice;
-
-    }
-
-    public static void main(String[] args) {
-
-        HomePrice homePrice = new HomePrice();
-
-        // Making list of people
-        List<Person> friendsInFarawayLands = new ArrayList<>();
-
-        LatLong latLong1 = new LatLong(57.077455, -2.775336);
-        LatLong latLong2 = new LatLong(46.862496, 103.846656);
-        LatLong latLong3 = new LatLong(31.539538, -110.756196);
-
-        Person harryPotter = new Person("Harry",latLong1 );
-
-        Person tuvanThroatSinger = new Person("Mr. Vibrato", latLong2);
-        Person doraTheExplorer = new Person("Dora", latLong3 );
-
-        friendsInFarawayLands.add(harryPotter);
-        friendsInFarawayLands.add(tuvanThroatSinger);
-        friendsInFarawayLands.add(doraTheExplorer);
-
-        // Making list of cities
-
-        LatLong torontoLatLong = new LatLong(43.653226, -79.383184);
-        LatLong seattleLatLong = new LatLong(47.606209, -122.332071);
-        LatLong kashmirLatLong = new LatLong(33.778175, 76.576171);
-
-        homePrice.addCity("Toronto", 1000000,torontoLatLong);
-        homePrice.addCity("Seattle", 700000,seattleLatLong);
-        homePrice.addCity("Kashmir", 130000,kashmirLatLong);
-
-        LatLong city1 = new LatLong(57, -2);
-        LatLong city2 = new LatLong(46, 103);
-        LatLong city3 = new LatLong(31, -110);
-
-        homePrice.findNearestCity(city1);
-        homePrice.findNearestCity(city2);
-        homePrice.findNearestCity(city3);
-
-        double avgPrice = homePrice.calculateAvgHomePrice(friendsInFarawayLands);
-        System.out.println(avgPrice);
 
     }
 
